1459bd4972c236c8273356ec262be22602dab8f1
farillite-ai <farillite@gmail.com>
2025-12-28 00:40:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ st.markdown("<div class='container'>", unsafe_allow_html=True)
 # ===== YOUTUBE EMBED (1:00 - 1:20) =====
 st.components.v1.html("""
 <iframe width="300" height="170"
-src="https://www.youtube.com/embed/VIDEO_ID?start=60&end=80&controls=0"
+src="https://www.youtube.com/embed/vgp0yZ8gaEs?start=60&end=80&controls=0"
 frameborder="0"
 allow="autoplay">
 </iframe>
